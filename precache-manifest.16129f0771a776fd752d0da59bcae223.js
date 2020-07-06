326926a80f9cda4573a12d9a24c79e4ef957246f
PatrickYang <azureroki@gmail.com>
2020-07-06 17:00:24 +0800

    Updates

---
+++ precache-manifest.16129f0771a776fd752d0da59bcae223.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26c365bd92e99f8c4008252a7939c39",
+    "url": "/React-Redux-todoList-demo/index.html"
+  },
+  {
+    "revision": "ef38e4b1bb54b517bd7a",
+    "url": "/React-Redux-todoList-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cd1b82bddabd6df6db31",
+    "url": "/React-Redux-todoList-demo/static/js/2.2c1570bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-todoList-demo/static/js/2.2c1570bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef38e4b1bb54b517bd7a",
+    "url": "/React-Redux-todoList-demo/static/js/main.4ed66177.chunk.js"
+  },
+  {
+    "revision": "08e34ad9eb1cfc0cceef",
+    "url": "/React-Redux-todoList-demo/static/js/runtime-main.22738c18.js"
+  }
+]);
